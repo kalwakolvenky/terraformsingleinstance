fe65c710f02530cc22d5422c48327b1d894da796
kalwakolvenky <61227310+kalwakolvenky@users.noreply.github.com>
2021-02-01 15:58:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -96,12 +96,12 @@ data "aws_ami" "my_ami" {
      #name_regex       = "^mavrick"
      owners           = ["946995831463"]
 }
-terraform {
-  backend "s3" {
-    bucket = "devops-backend"
-    key    = "myterraform.tfstate"
-    region = "us-east-1"
-  }
+# terraform {
+#   backend "s3" {
+#     bucket = "devops-backend"
+#     key    = "myterraform.tfstate"
+#     region = "us-east-1"
+#   }
 }
 
 resource "aws_instance" "web-1" {
